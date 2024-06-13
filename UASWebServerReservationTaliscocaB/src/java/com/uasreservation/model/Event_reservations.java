113843601eae0e9096bfd163d436b643582829c6
nathsteve13 <nathanaelsteven06@gmail.com>
2024-06-13 14:38:42 +0700

    crud all classes

--- UASWebServerReservationTaliscocaB/src/java/com/uasreservation/model/Event_reservations.java
+++ UASWebServerReservationTaliscocaB/src/java/com/uasreservation/model/Event_reservations.java
@@ -8,6 +8,212 @@ package com.uasreservation.model;
  *
  * @author natha
  */
-public class Event_reservations {
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Event_reservations extends MyModel{
+    private int account_id;
+    private int event_id;
+    private int quantity;
+    private double amount;
+    private String status;
+    private LocalDate claim_date;
+    private LocalDate claimed_date;
+    private LocalDate updated_at;
+    private LocalDate created_at;
+
+    public Event_reservations() {
+        this.account_id = 0;
+        this.event_id = 0;
+        this.quantity = 0;
+        this.amount = 0.0;
+        this.status = "";
+        this.claim_date = LocalDate.now();
+        this.claimed_date = LocalDate.now();
+        this.updated_at = LocalDate.now();
+        this.created_at = LocalDate.now();
+    }
+
+    public Event_reservations(int account_id, int event_id, int quantity, double amount, String status, LocalDate claim_date, LocalDate claimed_date, LocalDate updated_at, LocalDate created_at) {
+        this.account_id = account_id;
+        this.event_id = event_id;
+        this.quantity = quantity;
+        this.amount = amount;
+        this.status = status;
+        this.claim_date = claim_date;
+        this.claimed_date = claimed_date;
+        this.updated_at = updated_at;
+        this.created_at = created_at;
+    }
     
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public int getEvent_id() {
+        return event_id;
+    }
+
+    public void setEvent_id(int event_id) {
+        this.event_id = event_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getClaim_date() {
+        return claim_date;
+    }
+
+    public void setClaim_date(LocalDate claim_date) {
+        this.claim_date = claim_date;
+    }
+
+    public LocalDate getClaimed_date() {
+        return claimed_date;
+    }
+
+    public void setClaimed_date(LocalDate claimed_date) {
+        this.claimed_date = claimed_date;
+    }
+
+    public LocalDate getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDate updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public LocalDate getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDate created_at) {
+        this.created_at = created_at;
+    }
+
+    @Override
+    public void insertData() {
+        try {
+            if (!MyModel.conn.isClosed()) {
+                PreparedStatement sql = MyModel.conn.prepareStatement(
+                        "INSERT INTO event_reservations (account_id, event_id, quantity, amount, status, claim_date, claimed_date, updated_at, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                sql.setInt(1, this.account_id);
+                sql.setInt(2, this.event_id);
+                sql.setInt(3, this.quantity);
+                sql.setDouble(4, this.amount);
+                sql.setString(5, this.status);
+                sql.setDate(6, java.sql.Date.valueOf(this.claim_date));
+                sql.setDate(7, java.sql.Date.valueOf(this.claimed_date));
+                sql.setDate(8, java.sql.Date.valueOf(this.updated_at));
+                sql.setDate(9, java.sql.Date.valueOf(this.created_at));
+                sql.executeUpdate();
+                sql.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void updateData() {
+        try {
+            if (!MyModel.conn.isClosed()) {
+                PreparedStatement sql = MyModel.conn.prepareStatement(
+                        "UPDATE event_reservations SET account_id = ?, event_id = ?, quantity = ?, amount = ?, status = ?, claim_date = ?, claimed_date = ?, updated_at = ?, created_at = ? WHERE account_id = ? AND event_id = ?");
+                sql.setInt(1, this.account_id);
+                sql.setInt(2, this.event_id);
+                sql.setInt(3, this.quantity);
+                sql.setDouble(4, this.amount);
+                sql.setString(5, this.status);
+                sql.setDate(6, java.sql.Date.valueOf(this.claim_date));
+                sql.setDate(7, java.sql.Date.valueOf(this.claimed_date));
+                sql.setDate(8, java.sql.Date.valueOf(this.updated_at));
+                sql.setDate(9, java.sql.Date.valueOf(this.created_at));
+                sql.setInt(10, this.account_id);
+                sql.setInt(11, this.event_id);
+                sql.executeUpdate();
+                sql.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void deleteData() {
+        try {
+            if (!MyModel.conn.isClosed()) {
+                PreparedStatement sql = MyModel.conn.prepareStatement(
+                        "DELETE FROM event_reservations WHERE account_id = ? AND event_id = ?");
+                sql.setInt(1, this.account_id);
+                sql.setInt(2, this.event_id);
+                sql.executeUpdate();
+                sql.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public ArrayList<String> viewListData() {
+        ArrayList<String> collections = new ArrayList<>();
+        try {
+            statement = MyModel.conn.createStatement();
+            result = statement.executeQuery("SELECT * FROM event_reservations");
+
+            while (result.next()) {
+                Event_reservations tempReservation = new Event_reservations();
+                tempReservation.setAccount_id(result.getInt("account_id"));
+                tempReservation.setEvent_id(result.getInt("event_id"));
+                tempReservation.setQuantity(result.getInt("quantity"));
+                tempReservation.setAmount(result.getDouble("amount"));
+                tempReservation.setStatus(result.getString("status"));
+                tempReservation.setClaim_date(result.getDate("claim_date").toLocalDate());
+                tempReservation.setClaimed_date(result.getDate("claimed_date").toLocalDate());
+                tempReservation.setUpdated_at(result.getDate("updated_at").toLocalDate());
+                tempReservation.setCreated_at(result.getDate("created_at").toLocalDate());
+
+                collections.add(tempReservation.getAccount_id() + "-" + tempReservation.getEvent_id() + "-" + tempReservation.getQuantity() + "-" + tempReservation.getAmount() + "-" + tempReservation.getStatus() + "-" + tempReservation.getClaim_date() + "-" + tempReservation.getClaimed_date() + "-" + tempReservation.getUpdated_at() + "-" + tempReservation.getCreated_at());
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                if (result != null) result.close();
+                if (statement != null) statement.close();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return collections;
+    }
 }
--- UASWebServerReservationTaliscocaB/src/java/com/uasreservation/model/Parkings.java
+++ UASWebServerReservationTaliscocaB/src/java/com/uasreservation/model/Parkings.java
@@ -8,6 +8,194 @@ package com.uasreservation.model;
  *
  * @author natha
  */
-public class Parkings {
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Parkings extends MyModel{
+    private int id;
+    private String parking_area;
+    private String status;
+    private LocalDate available_date;
+    private int locations_id;
+    private double price;
+    private LocalDate updated_at;
+    private LocalDate created_at;
+
+    public Parkings() {
+        this.id = 0;
+        this.parking_area = "";
+        this.status = "";
+        this.available_date = LocalDate.now();
+        this.locations_id = 0;
+        this.price = 0.0;
+        this.updated_at = LocalDate.now();
+        this.created_at = LocalDate.now();
+    }
+
+    public Parkings(String parking_area, String status, LocalDate available_date, int locations_id, double price, LocalDate updated_at, LocalDate created_at) {
+        this.parking_area = parking_area;
+        this.status = status;
+        this.available_date = available_date;
+        this.locations_id = locations_id;
+        this.price = price;
+        this.updated_at = updated_at;
+        this.created_at = created_at;
+    }
     
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getParking_area() {
+        return parking_area;
+    }
+
+    public void setParking_area(String parking_area) {
+        this.parking_area = parking_area;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getAvailable_date() {
+        return available_date;
+    }
+
+    public void setAvailable_date(LocalDate available_date) {
+        this.available_date = available_date;
+    }
+
+    public int getLocations_id() {
+        return locations_id;
+    }
+
+    public void setLocations_id(int locations_id) {
+        this.locations_id = locations_id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDate updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public LocalDate getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDate created_at) {
+        this.created_at = created_at;
+    }
+
+    @Override
+    public void insertData() {
+        try {
+            if (!MyModel.conn.isClosed()) {
+                PreparedStatement sql = MyModel.conn.prepareStatement(
+                        "INSERT INTO parkings (parking_area, status, available_date, locations_id, price, updated_at, created_at) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                sql.setString(1, this.parking_area);
+                sql.setString(2, this.status);
+                sql.setDate(3, java.sql.Date.valueOf(this.available_date));
+                sql.setInt(4, this.locations_id);
+                sql.setDouble(5, this.price);
+                sql.setDate(6, java.sql.Date.valueOf(this.updated_at));
+                sql.setDate(7, java.sql.Date.valueOf(this.created_at));
+                sql.executeUpdate();
+                sql.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void updateData() {
+        try {
+            if (!MyModel.conn.isClosed()) {
+                PreparedStatement sql = MyModel.conn.prepareStatement(
+                        "UPDATE parkings SET parking_area = ?, status = ?, available_date = ?, locations_id = ?, price = ?, updated_at = ?, created_at = ? WHERE id = ?");
+                sql.setString(1, this.parking_area);
+                sql.setString(2, this.status);
+                sql.setDate(3, java.sql.Date.valueOf(this.available_date));
+                sql.setInt(4, this.locations_id);
+                sql.setDouble(5, this.price);
+                sql.setDate(6, java.sql.Date.valueOf(this.updated_at));
+                sql.setDate(7, java.sql.Date.valueOf(this.created_at));
+                sql.setInt(8, this.id);
+                sql.executeUpdate();
+                sql.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void deleteData() {
+        try {
+            if (!MyModel.conn.isClosed()) {
+                PreparedStatement sql = MyModel.conn.prepareStatement(
+                    "DELETE FROM parkings WHERE id = ?");
+                sql.setInt(1, this.id);
+                sql.executeUpdate();
+                sql.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public ArrayList<String> viewListData() {
+        ArrayList<String> collections = new ArrayList<>();
+        try {
+            statement = MyModel.conn.createStatement();
+            result = statement.executeQuery("SELECT * FROM parkings");
+
+            while (result.next()) {
+                Parkings tempParking = new Parkings();
+                tempParking.setId(result.getInt("id"));
+                tempParking.setParking_area(result.getString("parking_area"));
+                tempParking.setStatus(result.getString("status"));
+                tempParking.setAvailable_date(result.getDate("available_date").toLocalDate());
+                tempParking.setLocations_id(result.getInt("locations_id"));
+                tempParking.setPrice(result.getDouble("price"));
+                tempParking.setUpdated_at(result.getDate("updated_at").toLocalDate());
+                tempParking.setCreated_at(result.getDate("created_at").toLocalDate());
+
+                collections.add(tempParking.getId() + "-" + tempParking.getParking_area() + "-" + tempParking.getStatus() + "-" + tempParking.getAvailable_date() + "-" + tempParking.getLocations_id() + "-" + tempParking.getPrice() + "-" + tempParking.getUpdated_at() + "-" + tempParking.getCreated_at());
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                if (result != null) result.close();
+                if (statement != null) statement.close();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return collections;
+    }
 }
