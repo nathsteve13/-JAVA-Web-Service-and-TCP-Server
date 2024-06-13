e730bea2105b8b7765be03274df57fd0ad5213be
nathsteve13 <nathanaelsteven06@gmail.com>
2024-06-13 15:24:49 +0700

    update operation untuk class Account

--- UASWebServerReservationTaliscocaB/src/java/com/uasreservation/services/reservationServices.java
+++ UASWebServerReservationTaliscocaB/src/java/com/uasreservation/services/reservationServices.java
@@ -4,6 +4,9 @@
  */
 package com.uasreservation.services;
 
+import com.uasreservation.model.Account;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -15,11 +18,49 @@ import javax.jws.WebParam;
 @WebService(serviceName = "reservationServices")
 public class reservationServices {
 
+    Account a;
+    
     /**
-     * This is a sample web service operation
+     * Web service operation
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @WebMethod(operationName = "insertDataAccount")
+    public void insertDataAccount(@WebParam(name = "name") String name, @WebParam(name = "dob") LocalDate dob, @WebParam(name = "email") String email, @WebParam(name = "username") String username, @WebParam(name = "password") String password, @WebParam(name = "balance") double balance, @WebParam(name = "updated_at") LocalDate updated_at, @WebParam(name = "created_at") LocalDate created_at) {
+        a = new Account(name, dob, email, username, password, balance, updated_at, created_at);
+        a.insertData();
+    }
+    
+    @WebMethod(operationName = "updateDataAccount")
+    public void updateDataAccount(@WebParam(name = "name") String name, @WebParam(name = "dob") LocalDate dob, @WebParam(name = "email") String email, @WebParam(name = "username") String username, @WebParam(name = "password") String password, @WebParam(name = "balance") double balance, @WebParam(name = "updated_at") LocalDate updated_at, @WebParam(name = "created_at") LocalDate created_at) {
+        a = new Account(name, dob, email, username, password, balance, updated_at, created_at);
+        a.updateData();
+    }
+
+    @WebMethod(operationName = "deleteDataAccount")
+    public void deleteDataAccount(@WebParam(name = "id") int id) {
+        a = new Account();
+        a.setId(id);
+        a.deleteData();
+    }
+    
+    @WebMethod(operationName = "viewListDataVehicle")
+    public ArrayList<String> viewListDataAccount() {
+        a = new Account();
+        return a.viewListData();
+    }
+
+    @WebMethod(operationName = "checkLogin")
+    public boolean checkLogin(@WebParam(name = "email") String email, @WebParam(name = "password") String password) {
+        a = new Account();
+        a.setEmail(email);
+        a.setPassword(password);
+        return a.checkLogin();
+    }
+    
+    @WebMethod(operationName = "checkEmail")
+    public boolean checkEmail(@WebParam(name = "email") String email, @WebParam(name = "password") String password) {
+        a = new Account();
+        a.setEmail(email);
+        a.setPassword(password);
+        return a.checkEmail();
     }
 }
