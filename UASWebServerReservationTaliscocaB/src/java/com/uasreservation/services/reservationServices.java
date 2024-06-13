715ca0952c578df90674c98fe688ccfdc8931840
nathsteve13 <nathanaelsteven06@gmail.com>
2024-06-13 15:08:42 +0700

    pindah package

--- UASWebServerReservationTaliscocaB/src/java/com/uasreservation/services/reservationServices.java
+++ UASWebServerReservationTaliscocaB/src/java/com/uasreservation/services/reservationServices.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
  */
-package com.uasreservation.model;
+package com.uasreservation.services;
 
 import javax.jws.WebService;
 import javax.jws.WebMethod;
